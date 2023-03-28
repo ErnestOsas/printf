fd06b6eacbe3bc8effc662e3aacf22a2046069a8
Ernest Akibor <akiborernest@gmail.com>
2023-03-28 13:59:03 -0700

    This is for the print-int.c file

--- main.h
+++ main.h
@@ -45,11 +45,16 @@ int _printf(const char *format, ...);
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],int flags, int width, int precision, int size);
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+long int convert_size_number(long int num, int size);
+
 
 #endif
