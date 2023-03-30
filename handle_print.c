51d7f998192cc6ef3bf47744d8b8032a872edc20
Ernest Akibor <akiborernest@gmail.com>
2023-03-30 01:38:22 -0700

    This is for the print_unsigned_hexa_upper.c

--- handle_print.c
+++ handle_print.c
@@ -17,7 +17,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary}, {'u', print_unsigned},
-	       	{'o', print_octal}
+	       	{'o', print_octal}, {'x', print_hexadecimal}, {'X', print_hexa_upper}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
--- main.h
+++ main.h
@@ -61,6 +61,8 @@ long int convert_size_number(long int num, int size);
 int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
-
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 #endif
