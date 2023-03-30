84d253b41adb32b5714ee995737e2ac9e43bd9c4
Ernest Akibor <akiborernest@gmail.com>
2023-03-30 02:47:07 -0700

    This is for the string with ASCII code value in Hexa(Upper)

--- main.h
+++ main.h
@@ -64,5 +64,8 @@ int print_octal(va_list types, char buffer[], int flags, int width, int precisio
 int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
+int append_hexa_code(char, char[], int);
+int is_printable(char);
 
 #endif
